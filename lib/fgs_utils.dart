3c57d045eb52f0a2f3080891a6254e8a96a449e7
Marco Aurélio Prado <marco.pdsv@gmail.com>
2023-05-23 14:24:29 -0300

    refactor: exporting service cases implementations

--- lib/fgs_utils.dart
+++ lib/fgs_utils.dart
@@ -3,8 +3,12 @@ library fgs_utils;
 
 export 'src/analytics/fgs_utils__analytics_service.dart';
 export 'src/analytics/service_implementations/firebase_analytics/fgs_utils__firebase_analytics__analytics_service_implementation.dart';
+export 'src/analytics/service_implementations/firebase_analytics/service_cases_implementations/fgs_utils__firebase_analytics__analytics__set_current_screen_for_analytics_service_case_implementation.dart';
 export 'src/log/fgs_utils__log_service.dart';
 export 'src/log/service_implementations/firebase_crashlytics/fgs_utils__firebase_crashlytics__log_service_implementation.dart';
+export 'src/log/service_implementations/firebase_crashlytics/service_cases_implementations/fgs_utils__firebase_crashlytics__log__log_fatal_error_service_case_implementation.dart';
+export 'src/log/service_implementations/firebase_crashlytics/service_cases_implementations/fgs_utils__firebase_crashlytics__log__log_info_service_case_implementation.dart';
+export 'src/log/service_implementations/firebase_crashlytics/service_cases_implementations/fgs_utils__firebase_crashlytics__log__log_non_fatal_error_service_case_implementation.dart';
 export 'src/navigable_widget/fgs_utils__navigable_state.dart';
 export 'src/navigable_widget/fgs_utils__navigable_widget_mixin.dart';
 export 'src/navigable_widget/fgs_utils__navigate_to_state.dart';
@@ -16,3 +20,5 @@ export 'src/toast_message_launcher_widget/fgs_utils__toast_message_state.dart';
 export 'src/translatable_message/fgs_utils__translateable_message_state.dart';
 export 'src/uuid/fgs_utils__uuid_service.dart';
 export 'src/uuid/service_implementations/default/fgs_utils__default__uuid_service_implementation.dart';
+export 'src/uuid/service_implementations/default/service_cases_implementations/fgs_utils__default__uuid__generate_v1_uuid_service_case_implementation.dart';
+export 'src/uuid/service_implementations/default/service_cases_implementations/fgs_utils__default__uuid__generate_v4_uuid_service_case_implementation.dart';
