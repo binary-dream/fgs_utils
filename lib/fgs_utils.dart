d8cb82be86998e299ea1e28047c1f4f5ecbd71bc
Marco Aurélio Prado <marco.pdsv@gmail.com>
2023-05-23 14:13:06 -0300

    fix: add missed changes

--- lib/fgs_utils.dart
+++ lib/fgs_utils.dart
@@ -1,10 +1,10 @@
 /// A Very Good Project created by Very Good CLI.
 library fgs_utils;
 
-export 'src/analytics/fgs_utils__analytics_service_cases.dart';
-export 'src/analytics/fgs_utils__firebase_analytics_service.dart';
-export 'src/log/fgs_utils__firebase_crashlytics_service.dart';
-export 'src/log/fgs_utils__log_service_cases.dart';
+export 'src/analytics/fgs_utils__analytics_service.dart';
+export 'src/analytics/service_implementations/firebase_analytics/fgs_utils__firebase_analytics__analytics_service_implementation.dart';
+export 'src/log/fgs_utils__log_service.dart';
+export 'src/log/service_implementations/firebase_crashlytics/fgs_utils__firebase_crashlytics__log_service_implementation.dart';
 export 'src/navigable_widget/fgs_utils__navigable_state.dart';
 export 'src/navigable_widget/fgs_utils__navigable_widget_mixin.dart';
 export 'src/navigable_widget/fgs_utils__navigate_to_state.dart';
@@ -15,4 +15,4 @@ export 'src/toast_message_launcher_widget/fgs_utils__toast_message_launcher_widg
 export 'src/toast_message_launcher_widget/fgs_utils__toast_message_state.dart';
 export 'src/translatable_message/fgs_utils__translateable_message_state.dart';
 export 'src/uuid/fgs_utils__uuid_service.dart';
-export 'src/uuid/fgs_utils__uuid_service_implementation.dart';
+export 'src/uuid/service_implementations/default/fgs_utils__default__uuid_service_implementation.dart';
